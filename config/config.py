7d1dc68615fb5c43c1c9218df9e32ba6acc51555
krisikas <demian17.03.2009@mail.ru>
2025-11-18 18:56:02 +0300

    com

---
+++ config/config.py
@@ -0,0 +1,73 @@
+import xml.etree.ElementTree as ET  # Модуль для обработки XML данных
+
+# Пути к рабочим файлам конфигурации
+simulationModels = '/home/clover/catkin_ws/src/clover/clover_simulation/models/'
+markerLaunch = '/home/clover/catkin_ws/src/clover/clover/launch/aruco.launch'
+droneLaunch = '/home/clover/catkin_ws/src/clover/clover/launch/clover.launch'
+environmentFile = '/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world'
+markerData = '/home/clover/catkin_ws/src/clover/aruco_pose/map/'
+
+
+
+
+# ========= Обработка файла мира (world)=========
+try:
+    # Чтение и анализ XML структуры мира
+    environmentData = ET.parse(environmentFile)
+    worldRoot = environmentData.getroot()
+    
+    # Получение информации о маркерах из конфигурации
+    markerInfo = worldRoot[0][2][0].text.split('_')
+    
+    # Открытие файла с данными о карте маркеров
+    mapFile = open(markerData + markerInfo[1] + '.' + markerInfo[2], 'r')
+    
+    # Извлечение параметра размера из файла карты
+    markerSize = mapFile.read().split('\n')[1].split('\t')[1]
+    
+except:
+    # Сообщение об отсутствии карты маркеров
+    print('Карта маркеров не обнаружена! Запустите genmap.py, подробности: https://clover.coex.tech/en/aruco_map.html#marker-map-definition')
+    exit()
+
+
+
+
+
+
+
+# ========= Настройка параметров системы маркеров aruco.launch =========
+markerConfig = ET.parse(markerLaunch)
+markerRoot = markerConfig.getroot()
+
+# Обновление параметров конфигурации маркеров
+for index in range(len(markerConfig.findall('arg'))):
+    if markerRoot[index].attrib['name'] in ['aruco_detect', 'aruco_map', 'aruco_vpe']:
+        markerRoot[index].attrib['default'] = 'true'  # Активация модулей работы с маркерами
+    elif markerRoot[index].attrib['name'] == 'placement':
+        markerRoot[index].attrib['default'] = 'floor'  # Расположение маркеров на полу
+    elif markerRoot[index].attrib['name'] == 'map':
+        markerRoot[index].attrib['default'] = markerInfo[1] + '.' + markerInfo[2]  # Имя файла карты
+    elif markerRoot[index].attrib['name'] == 'length':
+        markerRoot[index].attrib['default'] = markerSize  # Установка размера маркера
+
+# Сохранение обновленной конфигурации
+markerConfig.write(markerLaunch)
+
+
+
+
+
+# ========= Конфигурация основных параметров дрона clover.launch =========
+droneConfig = ET.parse(droneLaunch)
+droneRoot = droneConfig.getroot()
+
+# Настройка рабочих модулей системы
+for index in range(len(droneConfig.findall('arg'))):
+    if droneRoot[index].attrib['name'] in ['simulator', 'web_video_server', 'rosbridge', 'main_camera', 'optical_flow', 'aruco', 'rangefinder_vl53l1x', 'led']:
+        droneRoot[index].attrib['default'] = 'true'  # Включение основных систем
+    elif droneRoot[index].attrib['name'] in ['blocks', 'rc']:
+        droneRoot[index].attrib['default'] = 'false'  # Отключение вспомогательных модулей
+
+# Запись изменений в файл конфигурации
+droneConfig.write(droneLaunch)
---
+++ config/gen_tubes.py
@@ -0,0 +1,107 @@
+import random  # Импортируем модуль random для генерации случайных чисел
+import math
+
+# Список доступных цветов для зданий
+main_tube = 'model://main_tube'
+tube = 'model://tube'
+
+poss = [[f"{1+i*0.5} 4", ["1.57", "-1.57"]] for i in range(1, 9)] + [[f"{round(5+ i*0.5*math.cos(30*math.pi/180), 2)} {round(4-i*0.5*math.sin(30*math.pi/180), 2)}", ["1.05", "-2.09"]] for i in range(1, 9)]
+
+randPos = []   # Список для хранения случайно выбранных позиций зданий
+randTurn = [] # Список для хранения случайно выбранных цветов зданий
+
+# Цикл для выбора случайных позиций и цветов для пяти зданий
+for i in range(5):
+    p = poss[random.randrange(len(poss))] # Выбор случайной позиции из списка
+    poss.remove(p)                        # Удаление выбранной позиции из списка возможных
+    randTurn.append(p[1][random.randrange(2)]) # Выбор случайного цвета и сохранения в список
+    randPos.append(p[0])     # Форматирование позиции в строку и сохрание в список
+    print(i, randPos[len(randPos)-1], randTurn[len(randTurn)-1])
+
+# Открытие файла для записи настроек мира
+world = open('/home/clover/catkin_ws/src/clover/clover_simulation/resources/worlds/clover_aruco.world', 'w')
+
+# Запись содержимого файла
+world.write(f'''<?xml version="1.0" ?>
+<sdf version="1.5">
+  <world name="NTO">
+    <include>
+      <uri>model://sun</uri>
+    </include>
+
+    <include>
+      <uri>model://parquet_plane</uri>
+      <pose>0 0 -0.01 0 0 0</pose>
+    </include>
+
+    <include>
+      <uri>model://aruco_cmit_txt</uri>
+    </include>
+
+    <include>
+      <name>main_tube</name>
+      <uri>{main_tube}</uri>
+      <pose>1 4 0 0 0 1.57</pose>
+    </include>
+
+    <include>
+      <name>tube_n1</name>
+      <uri>{tube}</uri>
+      <pose>{randPos[0]} 0 0 0 {randTurn[0]}</pose>
+    </include>
+    <include>
+      <name>tube_n2</name>
+      <uri>{tube}</uri>
+      <pose>{randPos[1]} 0 0 0 {randTurn[1]}</pose>
+    </include>
+    <include>
+      <name>tube_n3</name>
+      <uri>{tube}</uri>
+      <pose>{randPos[2]} 0 0 0 {randTurn[2]}</pose>
+    </include>
+    <include>
+      <name>tube_n4</name>
+      <uri>{tube}</uri>
+      <pose>{randPos[3]} 0 0 0 {randTurn[3]}</pose>
+    </include>
+    <include>
+      <name>tube_n5</name>
+      <uri>{tube}</uri>
+      <pose>{randPos[4]} 0 0 0 {randTurn[4]}</pose>
+    </include>
+
+    <scene>
+      <ambient>0.8 0.8 0.8 1</ambient>
+      <background>0.8 0.9 1 1</background>
+      <shadows>false</shadows>
+      <grid>false</grid>
+      <origin_visual>false</origin_visual>
+    </scene>
+  
+    <physics name='default_physics' default='0' type='ode'>
+      <gravity>0 0 -9.8066</gravity>
+      <ode>
+        <solver>
+          <type>quick</type>
+          <iters>10</iters>
+          <sor>1.3</sor>
+          <use_dynamic_moi_rescaling>0</use_dynamic_moi_rescaling>
+        </solver>
+        <constraints>
+          <cfm>0</cfm>
+          <erp>0.2</erp>
+          <contact_max_correcting_vel>100</contact_max_correcting_vel>
+          <contact_surface_layer>0.001</contact_surface_layer>
+        </constraints>
+      </ode>
+      <max_step_size>0.004</max_step_size>
+      <real_time_factor>1</real_time_factor>
+      <real_time_update_rate>250</real_time_update_rate>
+      <magnetic_field>6.0e-6 2.3e-5 -4.2e-5</magnetic_field>
+    </physics>
+  </world>
+</sdf>
+
+''')
+
+world.close() # Закрытие файла после записи
